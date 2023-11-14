b97ba55d5959e8796ee4fc54519e4ec5836b75f2
elloko <ellokoprograma@gmail.com>
2023-11-14 21:22:24 +0200

    fully implemented the navbar

---
+++ src/ts/contact.ts
@@ -0,0 +1,6 @@
+// importing the navigation component
+import NavigationBarUI from "./custom/navbarui";
+
+// initializing the navigation component
+const navbar = new NavigationBarUI();
+navbar.initialize();
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ module.exports = {
   mode: "development", // "production" | "development" | "none"
   entry: {
     index: './src/ts/index.ts',        // Entry point for the index page codebase
-    // products: './src/ts/products.ts', // Entry point for the product page codebase
-    // services: './src/ts/services.ts', // Entry point for the services codebase
+    products: './src/ts/products.ts', // Entry point for the product page codebase
+    services: './src/ts/services.ts', // Entry point for the services codebase
+    contact: './src/ts/contact.ts', // Entry point for the contact codebase
     // adminlogin: './src/ts/admin/admin-login.ts', // Entry point for the admin -login page codebase
-    // admin: './src/ts/admin/admin', // Entry point for the admin page codebase
+    // admin: './src/ts/admin/admin.ts', // Entry point for the admin page codebase
   },
   output: {
     path: path.resolve(__dirname, 'public/js'),
