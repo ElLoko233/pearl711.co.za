7135721ef95e4c1949830650be2622ce4a0b4216
elloko <ellokoprograma@gmail.com>
2023-11-22 13:29:23 +0200

    completed the products page ui and impleneted the send email contact us page

--- src/ts/contact.ts
+++ src/ts/contact.ts
@@ -4,3 +4,27 @@ import NavigationBarUI from "./custom/navbarui";
 // initializing the navigation component
 const navbar = new NavigationBarUI();
 navbar.initialize();
+
+// getting the contact us form
+const contactForm : HTMLFormElement = document.getElementById("contactForm") as HTMLFormElement;
+
+// binding the submit event
+contactForm.addEventListener("submit", (event) => {
+    // preventing the default
+    event.preventDefault();
+
+    // owners email
+    const PearlEmail : string= "pearl.services25@gmail.com"
+
+    // getting the name
+    const name: string  = contactForm["userName"].value;
+
+    // getting the message
+    const message : string = contactForm["msg"].value;
+
+    // Construct the mailto link with the user's email and message
+    var mailtoLink = `mailto:${PearlEmail}?subject=Contact%20Us%20(${name})&body=${message}`;
+
+    // Open the default email client with the mailto link
+    window.location.href = mailtoLink;
+});
--- src/ts/products.ts
+++ src/ts/products.ts
@@ -4,3 +4,111 @@ import NavigationBarUI from "./custom/navbarui";
 // initializing the navigation component
 const navbar = new NavigationBarUI();
 navbar.initialize();
+
+window.addEventListener("load", () => {
+    // initializing the navigation component
+    const navbar = new NavigationBarUI();
+    navbar.initialize();
+
+
+    // getting the search button element
+    const searchButton : HTMLButtonElement = document.getElementById('searchButton') as HTMLButtonElement;
+    const searchButtonCloseIcon : HTMLElement = document.getElementById("closeSearch") as HTMLElement;
+    const searchButtonSearchIcon : HTMLElement = document.getElementById('searchIcon') as HTMLElement;
+
+    // getting the filter button element
+    const filterButton : HTMLButtonElement = document.getElementById('filterButton') as HTMLButtonElement;
+    
+    // getting the search form element
+    const searchForm : HTMLFormElement = document.getElementById('searchForm') as HTMLFormElement;
+    // getting the search input
+    const searchInput : HTMLInputElement = searchForm["search-product"] as HTMLInputElement;
+
+    // getting the filter form element
+    const filterForm : HTMLFormElement = document.getElementById('filters') as HTMLFormElement;
+    
+    // binding the click event on the search button
+    searchButton.addEventListener("click", ( event ) => {
+        // diabling default
+        event.preventDefault();
+    
+        // checking if the search form is hidden
+        if( searchForm.classList.contains("hidden")){
+            if( searchButtonCloseIcon && searchButtonSearchIcon ){
+                // changing the button icon to close
+                searchButtonCloseIcon.classList.remove('hidden');
+                searchButtonSearchIcon.classList.add('hidden');
+            }
+
+            // removing the hidden class
+            searchForm.classList.remove("hidden");
+        }else{
+            if( searchButtonCloseIcon && searchButtonSearchIcon ){
+                // changing the button icon to search
+                searchButtonCloseIcon.classList.add('hidden');
+                searchButtonSearchIcon.classList.remove('hidden');
+
+                // clearing the search input
+                searchInput.value = "";
+            }
+
+            // adding the hidden class
+            searchForm.classList.add("hidden");
+        }
+    });
+
+    // binding the click event on the filter button
+    filterButton.addEventListener("click", ( event ) => {
+        // diabling default
+        event.preventDefault();
+    
+        // checking if the search form is hidden
+        if( filterForm.classList.contains("hidden")){
+            // removing the hidden class
+            filterForm.classList.remove("hidden");
+        }else{
+            // adding the hidden class
+            filterForm.classList.add("hidden");
+
+            // resetting the form'
+            filterForm.reset();
+        }
+    });
+
+    // binding the input search 
+    searchForm.addEventListener('submit', (event) => {
+        // preving the defaul
+        event.preventDefault();
+
+        // getting the value input
+        const searchValue : string = searchInput.value;
+
+        console.log('searchValue :>> ', searchValue);
+    })
+
+    //  binding the filter form submit
+    filterForm.addEventListener('submit', (event) => {
+        // preving the defaul
+        event.preventDefault();
+
+        // getting the form inputs
+        const formInput: HTMLFormControlsCollection  = filterForm.elements;
+
+        //  creating container for the checked checkboxes
+        const checkedCheckboxes: Array<string> = [];
+
+        // looping through the form inputs
+        for (let index = 0; index < formInput.length; index++) {
+            // getting the current input
+            const input: HTMLInputElement = formInput[index] as HTMLInputElement;
+
+            // checking if the input is a checkbox
+            if( input.type === "checkbox" && input.checked ){
+                // pushing the checkbox value to the checkedCheckboxes array
+                checkedCheckboxes.push(input.value);
+            }
+        }
+
+        console.log('checkedCheckboxes :>> ', checkedCheckboxes);
+    })
+});
